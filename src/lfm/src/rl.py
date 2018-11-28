4c38fa8c410d0a1043f9c15e2ec6d43c23e3898e
Edward Terry <edward.william.terry@gmail.com>
2018-11-28 11:54:12 -0500

    fixed more bugs

--- src/lfm/src/rl.py
+++ src/lfm/src/rl.py
@@ -1,4 +1,5 @@
 import numpy as np
+np.random.seed(0)
 
 class Env():
     def __init__(self):
@@ -10,17 +11,18 @@ class Env():
         self.reward_noop_sel = 0.0
         self.max_num_moves = 10
         self.grid_state = np.zeros((self.grid_edges, self.grid_edges))
-        # self.block_pos = {1: (1, 1), 2: (1, 2), 3: (2, 1), 4: (2, 2)}
-        self.block_pos = {1: (0, 0), 2: (0, 3), 3: (3, 0), 4: (3, 3)}
+        self.block_pos_orig = {1: (1, 1), 2: (1, 2), 3: (2, 1), 4: (2, 2)}
+        self.block_pos = {1: (1, 1), 2: (1, 2), 3: (2, 1), 4: (2, 2)}
+        # self.block_pos = {1: (0, 0), 2: (2, 3), 3: (3, 0), 4: (3, 3)}
         self.move_count = 0
         self.action_map = {     'N':  (-1,  0), # (r, c)
-                                'NW': (-1, -1),
+                                # 'NW': (-1, -1),
                                 'W':  ( 0, -1),
-                                'SW': ( 1, -1),
+                                # 'SW': ( 1, -1),
                                 'S':  ( 1,  0),
-                                'SE': ( 1,  1),
+                                # 'SE': ( 1,  1),
                                 'E':  ( 0,  1),
-                                'NE': (-1,  1),
+                                # 'NE': (-1,  1),
                                 'NO_SEL': ( 0,  0),
                                 'NO_REQ': ( 0,  0)}
         self.reward_captured = {1: False, 2: False, 3: False, 4: False}
@@ -28,20 +30,22 @@ class Env():
 
     def reset_env(self):
         self.grid_state = np.zeros((self.grid_edges, self.grid_edges))
-        for block, pos in self.block_pos.items():
+        for block, pos in self.block_pos_orig.items():
+            self.block_pos[block] = (pos[0], pos[1])
             self.grid_state[pos[0]][pos[1]] = block
+            self.reward_captured[block] = False
 
         self.move_count = 0
-        pass
+        self.done = False
 
     def calc_reward(self, action):
-        reward = 0
+        _reward = 0
         if action == 'NO_SEL':
-            reward = reward + self.reward_noop_sel
+            _reward = _reward + self.reward_noop_sel
         else:
-            reward = reward + self.reward_per_move
-        for block in self.block_pos:
-            if not self.reward_captured[block]:
+            _reward = _reward + self.reward_per_move
+        if action == 'I_TERM' or self.done == True:
+            for block in self.block_pos:
                 free = []
                 for dir in self.action_map.keys():
                     action = self.action_map[dir]
@@ -51,17 +55,17 @@ class Env():
                             free.append(True)
                         elif not self.on_grid(_next):
                             free.append(True)
-                        else:
+                        else: # there's another neighboring block
                             free.append(False)
                 if all(v == True for v in free):
                     self.reward_captured[block] = True
-                    reward = reward + self.reward_sep
+                    _reward = _reward + self.reward_sep
 
-        if all(v == True for v in self.reward_captured.values()):
-            self.done = True
-            reward = reward + self.reward_end
+            if all(v == True for v in self.reward_captured.values()):
+                self.done = True
+                _reward = _reward + self.reward_end
 
-        return reward
+        return _reward
 
     def on_grid(self, coord):
         row = coord[0]
@@ -106,23 +110,37 @@ class Env():
                 self.block_pos[block] = (self.block_pos[block][0] + self.action_map['NO_REQ'][0], self.block_pos[block][1] + self.action_map['NO_REQ'][1])
                 self.grid_state[self.block_pos[block][0]][self.block_pos[block][1]] = block   
 
-    def episode_done(self):
-        return self.done or self.move_count >= self.max_num_moves
-
     def print_grid(self):
         print self.grid_state
 
     def act(self, block, action):
-        self.move_count = self.move_count + 1
-        self.move(block, action)
-        self.print_grid()
+        if ((self.move_count + 1) <= self.max_num_moves) and (not action == 'I_TERM'):
+            self.move_count = self.move_count + 1
+            self.move(block, action)
+        else:
+            self.done = True
+        # print "Action: ", action, " on block ", block
+        # self.print_grid()
         
-        return self.grid_state, self.calc_reward(action), self.episode_done()
+        return self.grid_state, self.block_pos, self.calc_reward(action), self.done
 
 if __name__ == "__main__":
     env = Env()
     env.reset_env()
-    block = 2#int(raw_input())
-    action = 'NO_SEL'#raw_input()
-    state, reward, done = env.act(block, action)
-    print reward
+    max_num_episodes = 10000
+    avail_actions = [k for k in env.action_map.keys() if not k=='NO_REQ']
+    avail_actions.append('I_TERM')
+    reward_array = []
+    episode = 0
+    while episode < max_num_episodes:
+        done = False
+        reward_total = 0
+        # print "\nNew episode!"
+        while not done:
+            state, block_pos, reward, done = env.act(np.random.choice(env.num_blocks)+1, np.random.choice(avail_actions))
+            # print reward
+            reward_total = reward_total + reward
+        episode = episode + 1
+        env.reset_env()
+        reward_array.append(reward_total)
+    print np.mean(reward_array)
