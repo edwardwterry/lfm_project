1fda05340fa48d7974d99ca9210cdbcc926e8170
Edward Terry <edward.william.terry@gmail.com>
2018-11-28 13:46:32 -0500

    model is training, deciding to quit early

--- src/lfm/src/rl.py
+++ src/lfm/src/rl.py
@@ -1,4 +1,13 @@
+# -*- coding: utf-8 -*-
+import random
 import numpy as np
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+
+EPISODES = 10000
+
 import matplotlib.pyplot as plt
 np.random.seed(0)
 
@@ -6,9 +15,9 @@ class Env():
     def __init__(self):
         self.grid_edges = 4
         self.num_blocks = 4
-        self.reward_per_move = -1.0
-        self.reward_sep = 1.0
-        self.reward_end = 10.0
+        self.reward_per_move = -0.1
+        self.reward_sep = 0.1
+        self.reward_end = 1.0
         self.reward_noop_sel = 0.0
         self.max_num_moves = 10
         self.grid_state = np.zeros((self.grid_edges, self.grid_edges))
@@ -21,9 +30,9 @@ class Env():
                                 # 'SW': ( 1, -1),
                                 'S':  ( 1,  0),
                                 # 'SE': ( 1,  1),
-                                'E':  ( 0,  1),
+                                'E':  ( 0,  1)}
                                 # 'NE': (-1,  1),
-                                'NO_SEL': ( 0,  0)}
+                                # 'NO_SEL': ( 0,  0)}
                                 # 'NO_REQ': ( 0,  0)}
         self.reward_captured = {1: False, 2: False, 3: False, 4: False}
         self.done = False
@@ -42,23 +51,23 @@ class Env():
 
     def calc_reward(self, action):
         _reward = 0
-        if action == 'NO_SEL':
-            _reward = _reward + self.reward_noop_sel
-        else:
-            _reward = _reward + self.reward_per_move
+        # if action == 'NO_SEL':
+        #     _reward = _reward + self.reward_noop_sel
+        # else:
+        _reward = _reward + self.reward_per_move
         if action == 'I_TERM' or self.done == True:
             for block in self.block_pos:
                 free = []
                 for dir in self.action_map.keys():
                     action = self.action_map[dir]
-                    if not dir == 'NO_SEL':# and not dir == 'NO_REQ':
-                        _next = (self.block_pos[block][0] + action[0], self.block_pos[block][1] + action[1])
-                        if self.on_grid(_next) and self.grid_state[_next[0]][_next[1]] == 0:
-                            free.append(True)
-                        elif not self.on_grid(_next):
-                            free.append(True)
-                        else: # there's another neighboring block
-                            free.append(False)
+                    # if not dir == 'NO_SEL':# and not dir == 'NO_REQ':
+                    _next = (self.block_pos[block][0] + action[0], self.block_pos[block][1] + action[1])
+                    if self.on_grid(_next) and self.grid_state[_next[0]][_next[1]] == 0:
+                        free.append(True)
+                    elif not self.on_grid(_next):
+                        free.append(True)
+                    else: # there's another neighboring block
+                        free.append(False)
                 if all(v == True for v in free):
                     self.reward_captured[block] = True
                     _reward = _reward + self.reward_sep
@@ -81,11 +90,11 @@ class Env():
             return True
 
     def move(self, block, action):
-        if action == 'NO_SEL':
-            return
+        # if action == 'NO_SEL':
+        #     return
         next_coord = (self.block_pos[block][0] + self.action_map[action][0], self.block_pos[block][1] + self.action_map[action][1])
         if not self.on_grid(next_coord): # move would take you over the edge
-            self.block_pos[block] = (self.block_pos[block][0] + self.action_map['NO_SEL'][0], self.block_pos[block][1] + self.action_map['NO_SEL'][1])
+            self.block_pos[block] = (self.block_pos[block][0], self.block_pos[block][1])
             self.grid_state[self.block_pos[block][0]][self.block_pos[block][1]] = block   
         elif (self.on_grid(next_coord) and self.grid_state[next_coord[0]][next_coord[1]] == 0): # move is on board and neighbor is free
             self.grid_state[self.block_pos[block][0]][self.block_pos[block][1]] = 0
@@ -109,13 +118,13 @@ class Env():
                 self.block_pos[block] = (self.block_pos[block][0] + self.action_map[action][0], self.block_pos[block][1] + self.action_map[action][1])
                 self.grid_state[self.block_pos[block][0]][self.block_pos[block][1]] = block                   
             else: # gridlock, can't move at all
-                self.block_pos[block] = (self.block_pos[block][0] + self.action_map['NO_SEL'][0], self.block_pos[block][1] + self.action_map['NO_SEL'][1])
+                self.block_pos[block] = (self.block_pos[block][0], self.block_pos[block][1])
                 self.grid_state[self.block_pos[block][0]][self.block_pos[block][1]] = block   
 
     def print_grid(self):
-        print self.grid_state
+        print (self.grid_state)
 
-    def act(self, block, action):
+    def step(self, block, action):
         if ((self.move_count + 1) <= self.max_num_moves) and (not action == 'I_TERM'):
             self.move_count = self.move_count + 1
             self.move(block, action)
@@ -126,50 +135,46 @@ class Env():
         
         return self.grid_state, self.block_pos, self.calc_reward(action), self.done
 
-if __name__ == "__main__":
-    env = Env()
-    env.reset()
-    max_num_episodes = 1000
-    avail_actions = [k for k in env.action_map.keys()]# if not k=='NO_REQ']
-    avail_actions.append('I_TERM')
-    reward_array = []
-    episode = 0
-    while episode < max_num_episodes:
-        done = False
-        reward_total = 0
-        # print "\nNew episode!"
-        while not done:
-            state, block_pos, reward, done = env.act(np.random.choice(env.num_blocks)+1, np.random.choice(avail_actions))
-            # print reward
-            reward_total = reward_total + reward
-        episode = episode + 1
-        env.reset()
-        reward_array.append(reward_total)
-    plt.scatter(range(max_num_episodes),reward_array)
-    plt.show()
-    # print np.mean(reward_array)
+# if __name__ == "__main__":
+#     env = Env()
+#     env.reset()
+#     max_num_episodes = 1000
+#     avail_actions = [k for k in env.action_map.keys()]# if not k=='NO_REQ']
+#     avail_actions.append('I_TERM')
+#     reward_array = []
+#     episode = 0
+#     while episode < max_num_episodes:
+#         done = False
+#         reward_total = 0
+#         # print "\nNew episode!"
+#         while not done:
+#             state, block_pos, reward, done = env.step(np.random.choice(env.num_blocks)+1, np.random.choice(avail_actions))
+#             # print reward
+#             reward_total = reward_total + reward
+#         episode = episode + 1
+#         env.reset()
+#         reward_array.append(reward_total)
+#     # plt.scatter(range(max_num_episodes),reward_array)
+#     # plt.show()
+#     # print np.mean(reward_array)
 
-# -*- coding: utf-8 -*-
-import random
-import numpy as np
-from collections import deque
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import Adam
 
-EPISODES = 1000
 
 class DQNAgent:
     def __init__(self, state_size, action_size):
         self.state_size = state_size
         self.action_size = action_size
         self.memory = deque(maxlen=2000)
-        self.gamma = 0.95    # discount rate
+        self.gamma = 0.99    # discount rate
         self.epsilon = 1.0  # exploration rate
-        self.epsilon_min = 0.01
-        self.epsilon_decay = 0.995
-        self.learning_rate = 0.001
+        self.epsilon_min = 0.05
+        self.epsilon_decay = 0.9999
+        self.learning_rate = 0.0001
         self.model = self._build_model()
+        self.action_map = ((1, 'N'), (1, 'W'), (1, 'S'), (1, 'E'),
+                           (2, 'N'), (2, 'W'), (2, 'S'), (2, 'E'),
+                           (3, 'N'), (3, 'W'), (3, 'S'), (3, 'E'),
+                           (4, 'N'), (4, 'W'), (4, 'S'), (4, 'E'), (1, 'I_TERM'))
 
     def _build_model(self):
         # Neural Net for Deep-Q learning Model
@@ -188,10 +193,12 @@ class DQNAgent:
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_size)
         act_values = self.model.predict(state)
+        # return np.argmax(act_values[0])  # returns action
         return np.argmax(act_values[0])  # returns action
 
     def replay(self, batch_size):
         minibatch = random.sample(self.memory, batch_size)
+        # print (minibatch)
         for state, action, reward, next_state, done in minibatch:
             target = reward
             if not done:
@@ -199,6 +206,7 @@ class DQNAgent:
                           np.amax(self.model.predict(next_state)[0]))
             target_f = self.model.predict(state)
             target_f[0][action] = target
+            # print (state, target_f)
             self.model.fit(state, target_f, epochs=1, verbose=0)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
@@ -213,28 +221,35 @@ class DQNAgent:
 if __name__ == "__main__":
     env = Env()
     state_size = len(env.grid_state.ravel())
-    action_size = len(env.action_map) + 1 # adds I_TERM
+    action_size = env.num_blocks * len(env.action_map) + 1 # adds I_TERM
     agent = DQNAgent(state_size, action_size)
     # agent.load("./save/cartpole-dqn.h5")
-    done = False
+    # done = False
     batch_size = 32
 
     for e in range(EPISODES):
         state = env.reset()
         state = np.reshape(state, [1, state_size])
-        for time in range(500):
-            # env.render()
+        done = False
+        if e%50 == 0:
+            reward_total = 0
+        # print ("here")
+        while not done:
             action = agent.act(state)
-            next_state, reward, done, _ = env.step(action)
-            # reward = reward if not done else -10
+            # print (agent.action_map[action][0], agent.action_map[action][1])
+            # input()
+            next_state, _, reward, done, = env.step(agent.action_map[action][0], agent.action_map[action][1])
+            reward_total = reward_total + reward
             next_state = np.reshape(next_state, [1, state_size]) # turns into a row vector
             agent.remember(state, action, reward, next_state, done)
             state = next_state
             if done:
-                print("episode: {}/{}, score: {}, e: {:.2}"
-                      .format(e, EPISODES, time, agent.epsilon))
+                # print ("Reward: ", reward_total)
+                if e%50 == 0:
+                    print("episode: {}/{}, score: {:.2}, e: {:.2}"
+                        .format(e, EPISODES, reward_total/50, agent.epsilon))
                 break
-            if len(agent.memory) > batch_size:
-                agent.replay(batch_size)
+        if len(agent.memory) > batch_size:
+            agent.replay(batch_size)
         # if e % 10 == 0:
         #     agent.save("./save/cartpole-dqn.h5")
